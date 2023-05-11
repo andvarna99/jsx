054fc2cfdcd8981311f4d58b3e5aa241819e6d4d
Andrea Varnado <andrea.h.varnado@gmail.com>
2023-05-11 11:25:10 -0500

    this commit created some code using imports and exports to learn the syntax

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // 1) import React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 
 // 2) get reference to div with ID root
 const el = document.getElementById('root');
@@ -8,10 +9,5 @@ const el = document.getElementById('root');
 // 3) tell react to take control of that element
 const root = ReactDOM.createRoot(el);
 
-//4) create a component
-function App(){
-    return <input style={{border: '3px solid red'}} type="number" min={5} />;
-}
-
-//5) show the component on the screen
+//4) show the component on the screen
 root.render(<App/>);
