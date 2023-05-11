607f224682e0f9cf395dfb614be4e40a04f14129
Andrea Varnado <andrea.h.varnado@gmail.com>
2023-05-11 10:23:38 -0500

    this commit deleted some files we didn't need for a basic project and aded the index.js file to start learning reacat syntax

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
+// 1) import React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// 2) get reference to div with ID root
+const el = document.getElementById('root');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 3) tell react to take control of that element
+const root = ReactDOM.createRoot(el);
+
+//4) create a component
+function App(){
+    return <input style={{border: '3px solid red'}} type="number" min={5} />;
+}
+
+//5) show the component on the screen
+root.render(<App/>);
